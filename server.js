3c3041dd4a44371495d39ad63036b2ff03ed7063
Alex Merced <alex@alexmerced.dev>
2022-11-17 11:56:53 -0500

    Initial Setup is Complete

---
+++ server.js
@@ -0,0 +1,40 @@
+//////////////////////////////////
+// Dependencies
+//////////////////////////////////
+require("dotenv").config()
+const express = require("express")
+const morgan = require("morgan")
+const methodOverride = require("method-override")
+
+
+//////////////////////////////////
+// Application Object
+//////////////////////////////////
+const app = express()
+
+
+//////////////////////////////////
+// Middleware
+//////////////////////////////////
+app.use(morgan("dev"))
+app.use(methodOverride("_method"))
+app.use(express.urlencoded({extended: true}))
+app.use("/static", express.static("public"))
+
+
+//////////////////////////////////
+// Routes and Routers
+//////////////////////////////////
+app.get("/", (req, res) => {
+    res.send("Server is Working")
+})
+
+
+
+//////////////////////////////////
+// Server Listener
+//////////////////////////////////
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`)
+})
